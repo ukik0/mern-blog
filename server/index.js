a4fcc77c52d23aaff79d3ea5fb2067a995eca22d
Dima <ctaricc@mail.ru>
2022-09-04 15:17:13 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+dotenv.config();
+
+//Constants
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => res.send('Hello'))
+
+async function start() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cnml8oe.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+    );
+
+    app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const UserModel =  new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
+}, {timestamps: true})
+
+export default mongoose.model('User', UserModel)
